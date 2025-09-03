9a771cc2d71352ac724a456b6ebfe66e5e35f706
mcdowellsmurthy <146916814+mcdowellsmurthy@users.noreply.github.com>
2025-09-03 09:05:05 -0400

    Update for actions

--- vote/app.py
+++ vote/app.py
@@ -26,7 +26,6 @@ def hello():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
-
     vote = None
 
     if request.method == 'POST':
